4e1b9dc5780560c913e7cdd13308582582c4aa90
Jashanpreet Singh <72213670+JashanpreetSinghp09@users.noreply.github.com>
2023-03-25 20:12:35 -0400

    Sign Up Sucessfull
    Firebase authentication is setup, user can sign up using email and password. v1.1

--- PostAPIc/js/auth.js
+++ PostAPIc/js/auth.js
@@ -1,7 +1,36 @@
  // setting up firebase authentication for user registration
 
-<script>
-   const firebaseApp = firebase.initializeApp({ /* Firebase config */ });
-   const db = firebaseApp.firestore();
-   const auth = firebaseApp.auth();
-</script>
+
+const firebaseApp = firebase.initializeApp({ 
+apiKey: "AIzaSyCWs_SUCQ4uB_3KNV_YcGpeMRGb892mthc",
+authDomain: "postapic-1af5e.firebaseapp.com",
+projectId: "postapic-1af5e",
+storageBucket: "postapic-1af5e.appspot.com",
+messagingSenderId: "585042060228",
+appId: "1:585042060228:web:9062a72b7f3bfcb835ba91"
+
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+// Sign Up
+
+function SignUp() {
+   const email = document.getElementById("email").value;
+   const password = document.getElementById("password").value;
+   console.log(email, password);
+
+   firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+         // Signed in 
+         window.alert("You are Signed Up");
+         console.log(result);
+         // ...
+      })
+      .catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+        window.alert("Error: " + error.message);
+         // ..
+      });
+}
